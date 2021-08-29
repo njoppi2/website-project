602edb6cdfede962174e89fc4d360412835e1bbb
Nelson Joppi <njoppi2@gmail.com>
2021-08-29 16:19:32 -0300

    add background images to main pages

--- web-app/src/Components/Main/Home/index.js
+++ web-app/src/Components/Main/Home/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 import { NavLink } from 'react-router-dom';
 
 const Content = () => (
-  <div className='home'>
+  <div className='main home'>
     <h1>Welcome to my portfolio website</h1>
     <p> Feel free to browse around and learn more about me.</p>
   </div>
--- web-app/src/Components/Main/Sign-in/index.js
+++ web-app/src/Components/Main/Sign-in/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 import { NavLink } from 'react-router-dom';
 
 const SignIn = () => (
-  <div className='sign-in'>
+  <div className='main sign-in'>
     <h1>Contact Me</h1>
     <p>You can reach me via email: <strong>hello@example.com</strong></p>
   </div>
