f64e4a05c109dbb61dcd5a09ab72d87156a908e6
Nelson Joppi <njoppi2@gmail.com>
2021-08-30 18:17:56 -0300

    last ui fixes

--- web-app/src/Components/Main/Home/index.js
+++ web-app/src/Components/Main/Home/index.js
@@ -8,19 +8,19 @@ const Content = () => (
     <h1 class='home-h1'>MESMO ESTANDO LONGE</h1>
     <div class="form-opacity">
       <form class="form-inline">
-        <div>
+        <div class='label-input-container'>
             <label class="input-label" for="email">Check-in:</label>
             <input class="date-input" type="date" id="email" placeholder="Enter email" name="email"></input>
         </div>
-        <div>
-          <label class="input-label" for="pwd">Check-out</label>
+        <div class='label-input-container'>
+          <label class="input-label" for="pwd">Check-out:</label>
           <input type="date" class="date-input" id="pwd" placeholder="Enter password" name="pwd"></input>
         </div>
-        <div>
+        <div class='label-input-container'>
           <input type="number" id="numberInput1" class="number-input" min="1" max="50" ></input>
           <input type="number" id="numberInput2" class="number-input" min="1" max="50" ></input>
         </div>
-        <div>
+        <div class='label-input-container'>
           <button class='home-button' type="submit"><i ></i> Pesquisar</button>
         </div>
       </form> 
