6c1587f3d1016ff892f206f315e39a5576211706
HuanShan150 <huan150@hotmail.com>
2021-08-29 21:47:02 -0300

    home screen

--- web-app/src/Components/Main/Home/index.js
+++ web-app/src/Components/Main/Home/index.js
@@ -1,10 +1,30 @@
 import './styles.css';
 import { NavLink } from 'react-router-dom';
 
+
 const Content = () => (
   <div className='main home'>
-    <h1>Welcome to my portfolio website</h1>
-    <p> Feel free to browse around and learn more about me.</p>
+    <h2>SINTA-SE EM CASA</h2>
+    <h1>MESMO ESTANDO LONGE</h1>
+    <div class="form-opacity">
+      <form class="form-inline">
+        <div>
+            <label class="input-label" for="email">Check-in:</label>
+            <input class="date-input" type="date" id="email" placeholder="Enter email" name="email"></input>
+        </div>
+        <div>
+          <label class="input-label" for="pwd">Check-out</label>
+          <input type="date" class="date-input" id="pwd" placeholder="Enter password" name="pwd"></input>
+        </div>
+        <div>
+          <input type="number" id="numberInput1" class="number-input" min="1" max="50" ></input>
+          <input type="number" id="numberInput2" class="number-input" min="1" max="50" ></input>
+        </div>
+        <div>
+          <button type="submit"><i ></i> Pesquisar</button>
+        </div>
+      </form> 
+    </div>
   </div>
   );
 
